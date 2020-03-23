6ee8976fc5f24e819a7ae3440e6cd8618cd4acd4
duchovny1 <54773747+duchovny1@users.noreply.github.com>
2020-03-23 22:28:50 +0200

    add repo for combinatorial algorhitms and add permutation without repetition

---
+++ Combinatorial Algorithms/CombinatoricalAlgorithmsLab/ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{  
+    public class Program
+    {
+        static string[] elements;
+        static bool[] used;
+        static string[] permute;
+
+        static void Main(string[] args)
+        {
+            elements = Console.ReadLine().Split(" ").ToArray();
+            used = new bool[elements.Length];
+            permute = new string[elements.Length];
+
+
+            Permute(0);
+        }
+
+        static void Permute(int index)
+        {
+            if(index >= elements.Length)
+            {
+                Console.WriteLine(string.Join(" ", permute));
+            }
+            else
+            {
+                for (int i = 0; i < elements.Length; i++)
+                {
+                    if (!used[i])
+                    {
+                        used[i] = true;
+                        permute[index] = elements[i];
+                        Permute(index + 1);
+                        used[i] = false;
+                    }
+                }
+                
+            }
+        }
+    }
+}
